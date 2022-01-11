31b327eb02e3efe60ae2afaf204dcb45966af694
Oka Sigit <oka.sigit9@gmail.com>
2022-01-11 13:55:27 +0700

    add api route

--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,12 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
-});
+// Route::middleware('auth:api')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
+
+Route::get('/posts', 'PostsController@index');
+Route::post('/posts/store', 'PostsController@store');
+Route::get('/posts/{id?}', 'PostsController@show');
+Route::post('/posts/update/{id?}', 'PostsController@update');
+Route::delete('/posts/{id?}', 'PostsController@destroy');
