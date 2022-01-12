a7d9e772f036e3fea1d028cfbfac3fe2b21cd5f8
Oka Sigit <oka.sigit9@gmail.com>
2022-01-12 21:38:11 +0700

    fitur update data

---
+++ resources/js/components/posts/Edit.vue
@@ -0,0 +1,66 @@
+<template>
+    <div class="container mt-3">
+        <div class="row justify-content-center">
+            <div class="col-md-12">
+                <div class="card card-default">
+                    <div class="card-header">
+                        Edit Post
+                    </div>
+                    <div class="card-body">
+                        <form @submit.prevent="PostUpdate">
+                            <div class="form-group">
+                                <label for="title">Title</label>
+                                <input type="text" class="form-control" v-model="post.title" placeholder="Masukan Title">
+                                <div v-if="validation.title">
+                                    <div class="alert alert-danger mt-1" role="alert">
+                                        {{ validation.title[0] }}
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="form-group">
+                                <label for="content">Content</label>
+                                <textarea class="form-control" v-model="post.content" placeholder="Masukan content" rows="10"></textarea>
+                                <div v-if="validation.content">
+                                    <div class="alert alert-danger mt-1" role="alert">
+                                        {{ validation.content[0] }}
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="form-group">
+                                <button type="submit" class="btn btn-md btn-warning">Update</button>
+                                <button type="reset" class="btn btn-md btn-default">Reset</button>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    data(){
+        return{
+            post: {}, validation: []
+        }
+    },
+    created(){
+        let uri = `http://localhost:8000/api/posts/${this.$route.params.id}`;
+        this.axios.get(uri).then((response) => {
+            this.post = response.data.data;
+        });
+    },
+    methods:{
+        PostUpdate(){
+            let uri = `http://localhost:8000/api/posts/update/${this.$route.params.id}`;
+            this.axios.post(uri, this.post)
+                .then((response)=>{
+                    this.$router.push({name: 'posts'});
+                }).catch(error => {
+                    this.validation = error.response.data.data;
+                });
+        }
+    }
+}
+</script>
